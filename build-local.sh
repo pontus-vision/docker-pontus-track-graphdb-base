2fc967c0e234d5a1f42a83e8a84ae4a61d235ad3
Pontus <lmartins@pontusnetworks.com>
2019-05-07 19:08:28 +0100

    split the graphdb into a base, a nifi and gui images

---
+++ build-local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $DIR/base
+docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-base
+docker push pontusvisiongdpr/pontus-track-graphdb-base
+
+cd $DIR/full-graphdb-nifi
+docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-nifi
+docker push pontusvisiongdpr/pontus-track-graphdb-nifi
+
+#cd $DIR/full-graphdb-gui
+#docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-gui
+#docker push pontusvisiongdpr/pontus-track-graphdb-gui
+
+
