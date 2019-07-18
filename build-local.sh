fdda2f635fc61437babde489228de9feb93f57ca
dev <dev@pontusvision.com>
2019-07-18 10:05:42 +0100

    sync up

--- build-local.sh
+++ build-local.sh
@@ -3,7 +3,7 @@
 set -e 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR/base
-docker build  --rm . -t pontusvisiongdpr/pontus-track-graphdb-base
+docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-base
 
 cd $DIR/full-graphdb-nifi
 docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-nifi
