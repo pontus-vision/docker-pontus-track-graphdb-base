537501c989810c026ba969adb67d21ce223adf14
Leo Martins <lmartins@pontusvision.com>
2021-04-11 16:42:15 +0100

    wip

--- build-docker.sh
+++ build-docker.sh
@@ -5,29 +5,30 @@ set -e
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 export DOLLAR='$'
+cat $DIR/base/Dockerfile.template | envsubst > $DIR/base/Dockerfile
+cd $DIR/base
+docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-base:${TAG}
+
 cd $DIR/orientdb
 cat $DIR/orientdb/Dockerfile.template | envsubst > $DIR/orientdb/Dockerfile
 docker build  --rm  . -t pontusvisiongdpr/pontus-track-graphdb-odb:${TAG}
 
 cd $DIR/orientdb-pt
+cat $DIR/orientdb-pt/Dockerfile.template | envsubst > $DIR/orientdb-pt/Dockerfile
 docker build  --rm . -t pontusvisiongdpr/pontus-track-graphdb-odb-pt:${TAG}
 
-cat $DIR/base/Dockerfile.template | envsubst > $DIR/base/Dockerfile
-cd $DIR/base
-docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-base:${TAG}
+#cd $DIR/full-graphdb-nifi
+#docker build  --rm . -t pontusvisiongdpr/pontus-track-graphdb-nifi:${TAG}
 
-cd $DIR/full-graphdb-nifi
-docker build  --rm . -t pontusvisiongdpr/pontus-track-graphdb-nifi:${TAG}
+#cd $DIR/full-graphdb-nifi-pt
+#docker build  --rm . -t pontusvisiongdpr/pontus-track-graphdb-nifi-pt:${TAG}
 
-cd $DIR/full-graphdb-nifi-pt
-docker build  --rm . -t pontusvisiongdpr/pontus-track-graphdb-nifi-pt:${TAG}
-
+docker push pontusvisiongdpr/pontus-track-graphdb-base:${TAG}
 docker push pontusvisiongdpr/pontus-track-graphdb-odb:${TAG}
 docker push pontusvisiongdpr/pontus-track-graphdb-odb-pt:${TAG}
 
-docker push pontusvisiongdpr/pontus-track-graphdb-nifi-pt:${TAG}
-docker push pontusvisiongdpr/pontus-track-graphdb-nifi:${TAG}
-docker push pontusvisiongdpr/pontus-track-graphdb-base:${TAG}
+#docker push pontusvisiongdpr/pontus-track-graphdb-nifi-pt:${TAG}
+#docker push pontusvisiongdpr/pontus-track-graphdb-nifi:${TAG}
 
 #cd $DIR/full-graphdb-gui
 #docker build --rm . -t pontusvisiongdpr/pontus-track-graphdb-gui
