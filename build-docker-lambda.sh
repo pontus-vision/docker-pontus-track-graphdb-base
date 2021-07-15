a34af05ce12f42ddfd1fae307750fed48c0a32e4
Leo Martins <lmartins@pontusvision.com>
2021-07-15 22:58:46 +0100

    wip

--- build-docker-lambda.sh
+++ build-docker-lambda.sh
@@ -34,8 +34,10 @@ else
 
   docker build  --rm . -t pontus-track-graphdb-odb-lambda:${TAG}
   TIMESTAMP=$(date +%y%m%d_%H%M%S)
-  docker tag pontus-track-graphdb-odb-lambda:${TAG} ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-track-graphdb-odb-lambda:${TAG}-${TIMESTAMP}  
-  docker push ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-track-graphdb-odb-lambda:${TAG}-${TIMESTAMP}
+  docker tag pontus-track-graphdb-odb-lambda:${TAG} ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-track-graphdb-odb-lambda:${TAG}
+  docker push ${FORMITI_DEV_ACCOUNT}.dkr.ecr.eu-west-2.amazonaws.com/pontus-track-graphdb-odb-lambda:${TAG}
+  IMAGE_SHA=$(aws ecr describe-images --repository-name pontus-track-graphdb-odb-lambda --image-ids imageTag=${TAG} | jq -r '.imageDetails[0].imageDigest')
+  aws lambda update-function-code --function-name graphdb  --image-uri 558029316991.dkr.ecr.eu-west-2.amazonaws.com/pontus-track-graphdb-odb-lambda@${IMAGE_SHA}
 
 fi
 
